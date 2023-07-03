a6768ce4353d7e0e8d3132ac0db26a5e3fcd0fae
unknown <pasha.syd04@gmail.com>
2023-07-03 07:26:07 +0200

    fixed text size on phone and added git link icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prefixpostfix';
+
+
 }
 
